8bfc07595d14d0c928a49cdf7804cc419616b00a
Jake Heiser <jakeheiser1@gmail.com>
2016-10-12 19:34:11 -0700

    Offset commits

--- Sources/Core/IDResolver.swift
+++ Sources/Core/IDResolver.swift
@@ -18,7 +18,34 @@ public class IDResolver {
         }
     }
     
+    public static func resolve(commitish: String, in repository: Repository) -> Commit? {
+        let object: String
+        let offset: Int
+        if let offsetCharacter = commitish.characters.index(of: "~"),
+            let offsetInt = Int(commitish.substring(from: commitish.index(after: offsetCharacter))) {
+            object = commitish.substring(to: offsetCharacter)
+            offset = offsetInt
+        } else {
+            object = commitish
+            offset = 0
+        }
+        
+        guard let commit = resolve(object: object, in: repository) as? Commit else {
+            return nil
+        }
+        
+        var offsetCommit: Commit? = commit
+        for _ in 0 ..< offset {
+            offsetCommit = offsetCommit?.parent
+        }
+        return offsetCommit
+    }
+    
     public static func resolve(treeish: String, in repository: Repository) -> Tree? {
+        if treeish.contains("~") { // Offset commit (e.g. HEAD~2)
+            return resolve(commitish: treeish, in: repository)?.tree
+        }
+        
         guard let object = resolve(object: treeish, in: repository) else {
             return nil
         }
